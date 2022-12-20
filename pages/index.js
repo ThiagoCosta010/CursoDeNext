1078b461c9b3e1f3b92e6db58b9420febd7132b5
Thiago Costa <tigurrock@gmail.com>
2022-12-20 20:34:18 -0300

    att

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
+
       <h1>Hello World Next.js</h1>
     </div>
   )
--- pages/products/index.js
+++ pages/products/index.js
@@ -3,7 +3,6 @@ import Navbar from "../../components/Navbar"
 export default function product(){
     return (
         <>
-        <Navbar />   
 
         <h1>Pagina de produtos</h1>
         <Link href="/">Voltar</Link>
