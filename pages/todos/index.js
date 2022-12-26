5621b05f67e74e95202f15aace2f3f44b37f8664
Thiago Costa <tigurrock@gmail.com>
2022-12-26 09:12:09 -0300

    next

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/Todos.module.css'
+import Link from 'next/link'
 export async function getStaticProps(){
     const data = await fetch(`https://jsonplaceholder.typicode.com/todos`)
 
@@ -15,7 +16,7 @@ export default function todos({todos}){
             <h1>Tarefas para fazer:</h1>
             <ul className={styles.todoList}>
                 {todos.map((todo) => (
-                    <li key={todo.id}>{todo.title}</li>
+                    <li key={todo.id}>{todo.title} - <Link href={`/todos/${todo.id}`}>ver mais</Link></li>
                 ))}
 
             </ul>
